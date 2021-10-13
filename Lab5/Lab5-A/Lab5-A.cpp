6e58e52aa51595c475fea0d9feb2129f502e9e47
Insfef <stas.cookies@gmail.com>
2021-10-13 14:16:46 +0300

    Nep

---
+++ Lab5/Lab5-A/Lab5-A.cpp
@@ -0,0 +1,44 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#include "windows.h"
+int main(void) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double a, b, c, d, x, xx, 
+	//m,
+	n;
+
+	printf("Введите a >");
+	scanf("%lf", &a);
+	printf("Введите b >");
+	scanf("%lf", &b);
+	printf("Введите c >");
+	scanf("%lf", &c);
+	//printf("Введите m >");
+	//scanf("%lf", &m);
+
+	//m = xx * a + x * b + c;
+
+	double b2, db, x1, x2;
+	b2 = b * b;
+
+	d = b2 - 4 * a * c;
+
+	if (d >= 0) {
+
+		db = sqrt(d);
+
+		x1 = (-b + db) / (2 * a);
+		x2 = (-b - db) / (2 * a);
+		if (d > 0)
+			printf("x1=%6.3lf;  x2=%6.3lf\n", x1, x2);
+		else
+			printf("x1=%6.3lf;\n", x1);
+	}
+	else
+
+		printf("арбуз");
+
+
+}
