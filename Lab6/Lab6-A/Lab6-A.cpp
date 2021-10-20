5d0421506983ce200a6fbb9adc7bbff16191f26d
Insfef <stas.cookies@gmail.com>
2021-10-20 13:45:08 +0300

    Nep

---
+++ Lab6/Lab6-A/Lab6-A.cpp
@@ -0,0 +1,44 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "windows.h"
+
+int main(void) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int k, x, z;
+
+	int i = 0;
+
+	printf("Угадайте количество арбузов\n\n");
+	srand(time(0));
+
+	x = rand() % 100 + 1;
+	//printf("asdawd=%d\n\n", x);
+	
+
+	while (true)
+	{
+
+		printf("Введите количество арбузов >");
+		scanf("%d", &k);
+
+		if (k > x)
+			printf("Арбузов меньше\n\n", k);
+
+		if (k < x)
+			printf("Арбузов больше\n\n", k);
+
+		if (k == x)
+		{
+			printf("Вы угадали количество арбузов\n\n", k);
+			return 0;
+		}
+	
+	}
+
+
+
+}
---
+++ Lab6/Lab6-B/Lab6-B.cpp
@@ -0,0 +1,49 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "windows.h"
+
+int main(void) {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int x1, x2, x3, k, n, p1;
+
+	srand(time(0));
+
+	x3 = 0;
+
+	for (n = 0; n < 9; n++)
+	{
+
+		x1 = rand() % 10 + 1;
+
+		x2 = rand() % 10 + 1;
+
+		p1 = x1 * x2;
+
+		//printf("%d\n", p1);
+
+		printf("%d * %d = ", x1, x2);
+
+		scanf("%d", &k);
+
+		if (k == p1)
+		{
+			x3++;
+			printf(" верно \n\n");
+		}
+
+
+		else
+			printf(" ошибка \n\n");
+
+
+
+	}
+
+	printf("Количество верных ответов: %d из 10\n\n", x3);
+
+
+	
+}
