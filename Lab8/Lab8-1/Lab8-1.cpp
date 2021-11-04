22b15d0f4185add32e85fdf88cfab998d884bfe3
Insfef <stas.cookies@gmail.com>
2021-11-04 15:05:20 +0200

    Nep

---
+++ Lab8/Lab8-1/Lab8-1.cpp
@@ -0,0 +1,43 @@
+﻿#include "windows.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int Ar[200];   /* массив, который обрабатывается */
+int main(void)
+	{
+		SetConsoleCP(1251);
+		SetConsoleOutputCP(1251);
+
+		int i, j;  
+		int nn; 
+
+		srand(time(NULL));
+		/* заполнение массива случайными числами */
+		for (i = 0; i < 200; Ar[i++] = rand() % 100 - 50);
+		/* вывод начального массива */
+		printf("Начальный массив:\n");
+		for (i = 0; i < 200; printf("%3d  ", Ar[i++]));
+		putchar('\n');
+		putchar('\n');
+
+		for (nn = i = 0; i < 200; i++) { 
+			int ii = i + 1;
+			int iii = i - 1;
+				if (Ar[ii] == Ar[i] - 1 && i < 199 || Ar[ii] == Ar[i])
+				{
+					printf("%3d  ", i);
+					if (nn == 0)
+					nn = 1;
+				}
+				else 
+				{
+					if (nn == 1)
+					{
+						printf("%3d  \n", i);
+						nn = 0;
+					}
+				}
+		}
+
+	
+}
