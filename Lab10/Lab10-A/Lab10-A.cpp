34b045bb74fae43a702ad6b0f712af2e2e7dcff5
Insfef <stas.cookies@gmail.com>
2021-11-30 23:17:07 +0200

    nep

---
+++ Lab10/Lab10-A/Lab10-A.cpp
@@ -0,0 +1,38 @@
+﻿#include "windows.h"
+#include <iostream>
+#define  _CRT_SECURE_NO_WARNINGS
+using namespace std;
+int nep(int a, int b)
+{
+	char iffy;
+	if (a > b)
+		iffy = '>';
+	if (a == b)
+		iffy = '=';
+	if (a < b)
+		iffy = '<';
+
+	return iffy;
+}
+
+
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n, nn;
+	char m;
+	cout << "Введите два числа для сравнения" << endl << endl;
+	cout << "Введите первое число >> ";
+	cin >> n;
+	cout << endl << "Введите второе число >> ";
+	cin >> nn;
+
+	m = nep(n, nn);
+
+	cout << endl << "Результат: " << n << m << nn << endl;
+
+}
+
