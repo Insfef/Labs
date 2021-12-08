a674d13ad51bdfa54aadbb7858653e45b84f9142
Insfef <stas.cookies@gmail.com>
2021-12-08 16:32:14 +0200

    nep

--- РГЗ/РГЗ/РГЗ.cpp
+++ РГЗ/РГЗ/РГЗ.cpp
@@ -1,4 +1,5 @@
 ﻿
+#include "windows.h"
 #include <iostream>
 #include <chrono>
 //#include "windows.h"
@@ -6,8 +7,16 @@ using namespace std;
 
 #define M 1000	// Количество циклов
 #define N 10
-#define I 100
+#define I 10
 
+void print_arr(int arr[])
+{
+	for (int i = 0; i < N; i++)
+	{
+		printf("%3d", arr[i]);
+	}
+	printf("\n\n");
+}
 int fill_arr_nep(int arr[])
 {
 	for (int i = 0; i < N; i++)
@@ -20,7 +29,7 @@ int fill_arr_nepr(int arr[])
 {
 	for (int i = 0; i < N; i++)
 	{
-		arr[i] = I - i;
+		arr[i] = I - i - 1;
 	}
 	return arr[N];
 }
@@ -100,60 +109,86 @@ int counting_sort(int arr[], int num_arr[])
 
 int main()
 {
-	//const int arr_size = 100;	// Количество циклов
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
 	double dur_arr[M];
 	long F = 0;
 	double aver_dur = 0;
 	int s_arr[N];
 	int arr[N];
 	fill_arr(s_arr);
-	copy_arr(s_arr, arr);
-	for (int i = 0; i < N; i++)
-	{
-		printf("%3d", arr[i]);
-	}
-
 	printf("\n\n\n");
+	printf("Неупоряд. массив");
+	print_arr(s_arr);
 	for (int i = 0; i < M; i++)
 	{
 		copy_arr(s_arr, arr);
-		//system("pause");
-		//Sleep(1000);
-
 		auto start = chrono::high_resolution_clock::now();
-
-
-		//std::cout << "f(" << N << ") = " << fibonacci(N) << '\n';
 		shuttle_sort(arr);
-
-
 		auto end = chrono::high_resolution_clock::now();
 		chrono::duration<double> duration = end - start;
+		dur_arr[i] = duration.count();
+	}
+	// Вывод результата
+	for (int i = 0; i < M; i++)
+	{
+		if (i > 0) aver_dur += dur_arr[i];
+	}
+	printf("Отсорт. массив");
+	print_arr(arr);
 
-		//std::cout << "Duration " << i << " = " << duration.count() << "s\n";
+	printf("Среднее значение (неупоряд.) = %.7f s\n\n\n\n\n", aver_dur / (M - 1) );
+
+	fill_arr_nep(s_arr);
+	printf("Упоряд. массив");
+	print_arr(s_arr);
+	for (int i = 0; i < M; i++)
+	{
+		copy_arr(s_arr, arr);
+		auto start = chrono::high_resolution_clock::now();
+		shuttle_sort(arr);
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> duration = end - start;
+		dur_arr[i] = duration.count();
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		if (i > 0) aver_dur += dur_arr[i];
+	}
+	printf("Отсорт. массив");
+	print_arr(arr);
+	printf("\nСреднее значение (упоряд.) = %.7f s\n\n\n\n\n", aver_dur / (M - 1));
+
+
+
+	printf("Упоряд. обрат. массив");
+	fill_arr_nepr(s_arr);
+
+
+	print_arr(s_arr);
+	for (int i = 0; i < M; i++)
+	{
+		copy_arr(s_arr, arr);
+		auto start = chrono::high_resolution_clock::now();
+		shuttle_sort(arr);
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> duration = end - start;
 		dur_arr[i] = duration.count();
 	}
 
 	// Вывод результата
 	for (int i = 0; i < M; i++)
 	{
-		/*printf("Duration %d = %.7f s\n", i, dur_arr[i]);*/
 		if (i > 0) aver_dur += dur_arr[i];
 	}
-	printf("Duration a = %.7f s", aver_dur / (M - 1) );
+	printf("Отсорт. массив");
+	print_arr(arr);
+	printf("\nСреднее значение (упоряд. обрат.) = %.7f s\n\n\n\n\n", aver_dur / (M - 1));
+
 
 	printf("\n\n\n");
-	for (int i = 0; i < N; i++)
-	{
-		printf("%3d", arr[i]);
-	}
-	printf("\n");
-
-	printf("\n\n\n");
-
-
-
-
 
 
 
@@ -164,26 +199,61 @@ int main()
 	fill_csort_arr(num_arrc);
 	fill_arr(s_arrc);
 	copy_arr(s_arrc, arrc);
-
-	for (int i = 0; i < N; i++)
-	{
-		printf("%3d", arrc[i]);
-	}
-
-	printf("\n\n\n");
+	printf("Неупоряд. массив");
+	print_arr(s_arrc);
 	for (int i = 0; i < M; i++)
 	{
 		copy_arr(s_arrc, arrc);
-
 		auto start = chrono::high_resolution_clock::now();
-
-
 		counting_sort(arrc, num_arrc);
-
-
 		auto end = chrono::high_resolution_clock::now();
 		chrono::duration<double> duration = end - start;
+		dur_arr[i] = duration.count();
+	}
+	printf("Отсорт. массив");
+	print_arr(arrc);
+	// Вывод результата
+	for (int i = 0; i < M; i++)
+	{
+		if (i > 0) aver_dur += dur_arr[i];
+	}
+	printf("Среднее значение (неупоряд.) = %.7f s", aver_dur / (M - 1));
+	printf("\n\n\n\n\n");
 
+
+	fill_arr_nep(s_arrc);
+	printf("Упоряд. массив");
+	print_arr(s_arrc);
+	for (int i = 0; i < M; i++)
+	{
+		copy_arr(s_arrc, arrc);
+		auto start = chrono::high_resolution_clock::now();
+		counting_sort(arrc, num_arrc);
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> duration = end - start;
+		dur_arr[i] = duration.count();
+	}
+
+	// Вывод результата
+	printf("Отсорт. массив");
+	print_arr(arrc);
+	for (int i = 0; i < M; i++)
+	{
+		if (i > 0) aver_dur += dur_arr[i];
+	}
+	printf("Среднее значение (упоряд.) = %.7f s", aver_dur / (M - 1));
+	printf("\n\n\n\n\n");
+
+	fill_arr_nepr(s_arrc);
+	printf("Упоряд. обрат. массив");
+	print_arr(s_arrc);
+	for (int i = 0; i < M; i++)
+	{
+		copy_arr(s_arrc, arrc);
+		auto start = chrono::high_resolution_clock::now();
+		counting_sort(arrc, num_arrc);
+		auto end = chrono::high_resolution_clock::now();
+		chrono::duration<double> duration = end - start;
 		dur_arr[i] = duration.count();
 	}
 
@@ -192,14 +262,9 @@ int main()
 	{
 		if (i > 0) aver_dur += dur_arr[i];
 	}
-	printf("Duration a = %.7f s", aver_dur / (M - 1));
-
-	printf("\n\n\n");
-	for (int i = 0; i < N; i++)
-	{
-		printf("%3d", arrc[i]);
-	}
-	printf("\n");
+	printf("Отсорт. массив");
+	print_arr(arrc);
+	printf("\nСреднее значение (упоряд. обрат.) = %.7f s", aver_dur / (M - 1));
 
 	printf("\n\n\n");
 
