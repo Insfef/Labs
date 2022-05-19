8ee63c8a1c48936c1dabdef99f20eb97942f65aa
Insfef <stas.cookies@gmail.com>
2022-05-19 12:23:03 +0300

    nep

--- _Labs2/Lab7/Lab7-0/0_F.cpp
+++ _Labs2/Lab7/Lab7-0/0_F.cpp
@@ -1,4 +1,4 @@
-﻿#include "0_H.h"
+﻿#include "0_hdr.h"
 
 #define PRINT_TYPE 1 
 #define DEBUG
---
+++ _Labs2/Lab7/Lab7-0/0_hdr.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+struct lists {
+	int num;
+	char name[15];
+	char type;
+	float price;
+	int mink;
+};
+
+void prnt(lists[], FILE* f, int* k);
+
+void exec(lists nep[], FILE* f, int* k);
+
+void exec_rand(lists[], FILE* f, int* k);
+
+void add_beg(lists[], FILE* f, int* k);
+
+void add_end(lists[], FILE* f, int* k);
+
+void read_n(lists[], FILE* f, int* k, int n);
+
+int compa(const char* s1, const char* s2);
