28b9248086ca351625db38b20a4c2f9bd7632337
Insfef <stas.cookies@gmail.com>
2022-05-25 13:14:19 +0300

    nep

--- _Labs2/Lab8/Lab8.cpp
+++ _Labs2/Lab8/Lab8.cpp
@@ -17,41 +17,34 @@ int main(void) {
 		printf(" 6. Сортировать записи\n 7. Перенести список в файл\n 8. Создать новый список из файла \n 9. Выйти из программы\n\t>> ");
 
 		cin >> ans;
+		system("CLS");
 		putchar('\n');
 		switch (ans) {
 		case 1:
-			system("CLS");
 			crt(nep, k);
 			a = 1;
 			break;
 		case 2:
-			system("CLS");
 			prnt(nep, k);
 			break;
 		case 3:
-			system("CLS");
 			fnd(nep, k);
 			break;
 		case 4:
-			system("CLS");
 			add(nep, k);
 			break;
 		case 5:
-			system("CLS");
 			dlt(nep, k);
 			break;
 		case 6:
-			system("CLS");
 			srt(nep, k);
 			break;
 		case 7:
-			system("CLS");
 			fopen_s(&f, "t1.txt", "w");
 			wrt(nep, f, k);
 			fclose(f);
 			break;
 		case 8:
-			system("CLS");
 			fopen_s(&f, "t1.txt", "r");
 			nwl(nep, f, k);
 			fclose(f);
