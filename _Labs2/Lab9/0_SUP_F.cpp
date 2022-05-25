9ff42f49f9bb7f0f732a357d6d2af484fbc4fe0e
Insfef <stas.cookies@gmail.com>
2022-05-25 13:11:34 +0300

    nep

--- _Labs2/Lab9/0_SUP_F.cpp
+++ _Labs2/Lab9/0_SUP_F.cpp
@@ -33,12 +33,9 @@ void prnt_down()
 
 int compa(const char* s1, const char* s2)
 {
-	unsigned char uc1, uc2;
 	while (*s1 != NULL && *s1 == *s2) {
 		s1++;
 		s2++;
 	}
-	uc1 = (*(unsigned char*)s1);
-	uc2 = (*(unsigned char*)s2);
-	return ((uc1 < uc2) ? -1 : (uc1 > uc2));
+	return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
